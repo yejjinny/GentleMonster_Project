6041babb6aef3b849658baffa4ccfd038ac84e9f
eni1221 <yin8213@gmail.com>
2023-11-08 11:14:12 +0900

    23.11.08 11시14분 윤예인 수정

--- RealSemi/src/main/webapp/js/member/editMemberInfo/changePwd_key.js
+++ RealSemi/src/main/webapp/js/member/editMemberInfo/changePwd_key.js
@@ -52,7 +52,7 @@ function goPwdKey(ctxPath){
 						  
 						  if( json.isExists ){
 							  
-							 
+							  alert("비밀번호변경이 완료되었습니다. 로그인을 하세요. ");
 							 location.href=`${ctxPath}/register/login.gm`;
 							  
 							 
